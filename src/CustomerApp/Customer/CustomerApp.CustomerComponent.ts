81687951b0628a98b11315cb1465ad4835e40f40
janglavas <moshatist@gmail.com>
2019-05-17 13:32:08 +0200

    test2

--- src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
+++ src/CustomerApp/Customer/CustomerApp.CustomerComponent.ts
@@ -1,6 +1,7 @@
 import { Component, Injector } from '@angular/core';
 import { Customer } from './CustomerApp.model';
-
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { BaseLogger } from "../Utility/CustomerApp.Logger";
 
 @Component({
@@ -12,12 +13,48 @@ export class CustomerComponent {
   CustomerModel : Customer = new Customer();
   CustomerModels : Array<Customer> = new Array<Customer>();
   Logobject : BaseLogger = null;
-  constructor(_injector : Injector){
+  Disable: boolean = false;
+  constructor(_injector : Injector, public http:Http,
+  public httpc:HttpClient){
     this.Logobject = _injector.get("1");
     this.Logobject.Log();
   }
+
+  PosttoServer(){
+    //http://localhost:3000/Customers
+    //data transfer object
+    this.Disable = true;
+    var custdto: any = {};
+    custdto.CustomerCode = this.CustomerModel.CustomerCode;
+    custdto.CustomerName = this.CustomerModel.CustomerName;
+    custdto.CustomerAmount = this.CustomerModel.CustomerAmount;
+
+    this.httpc.post("http://localhost:3000/Customers",
+      custdto).
+      subscribe(res=>this.Success(res),res=>this.Error(res));
+  }
+  GetFromServer(){
+    this.httpc.get("http://localhost:3000/Customers").
+    subscribe(res=>this.SuccessGet(res),res=>this.Error(res));
+  }
+
+
+  Error(res) {
+    console.debug(res.json());
+  }
+  Success(res){
+    this.GetFromServer(); //get call to get data from server back
+  }
+  SuccessGet(res) {
+    this.CustomerModels = res;
+    this.Disable = false;
+    this.CustomerModel = new Customer();
+  }
+
+
   SelectCustomer(_selected:Customer){
     this.CustomerModel = _selected;
+
   }
 
   Add(){
--- src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
+++ src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { GridComponent } from "../Utility/CustomerApp.GridComponent";
 import { CustomerComponent } from './CustomerApp.CustomerComponent';
 import { CustomerRoutes } from '../Routing/CustomerApp.CustomerRouting';
 import { BaseLogger, DbLogger, ConsoleLogger } from "../Utility/CustomerApp.Logger";
+import { MyInterceptor } from '../Utility/Utility.HttpInterceptor'
 
 //Decorator
 @NgModule({
@@ -17,15 +20,18 @@ import { BaseLogger, DbLogger, ConsoleLogger } from "../Utility/CustomerApp.Logg
     RouterModule.forChild(CustomerRoutes),
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [
   {
-  provide: BaseLogger,
-  useClass: ConsoleLogger
+  provide: HTTP_INTERCEPTORS,
+  useClass: MyInterceptor,
+  multi : true,
   },
-  {provide : "1", useClass: DbLogger},
-  {provide : "2", useClass: ConsoleLogger},
+  //{provide : "1", useClass: DbLogger},
+  //{provide : "2", useClass: ConsoleLogger},
   ],
   bootstrap: [CustomerComponent]
 })
